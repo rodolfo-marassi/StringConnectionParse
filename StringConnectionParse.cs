c370b4dd6bef7bb48f910b3d49c8692dc2d3b153
rodolfo-marassi <rodolfomarassi66@gmail.com>
2022-01-15 18:40:45 +0100

    Add class string connection parse

---
+++ StringConnectionParse.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace ReportCCPChekBilance
+{
+    public class StringConnectionParse
+    {
+        //Restituisce i valori
+        public readonly List<Parser> Parses = new List<Parser>();
+
+        /// <summary>
+        ///     Passare tutta la stringa di connessione
+        /// </summary>
+        /// <param name="ConnectionString"></param>
+        public void Parse(string ConnectionString)
+        {
+            var splitOne = ConnectionString.Split(';');
+
+            foreach (var t in splitOne)
+            {
+                var splitTwo = t.Split('=');
+
+                var parser = new Parser {Parametro = splitTwo[0], Valore = splitTwo[1]};
+
+                Parses.Add(parser);
+            }
+        }
+
+        public class Parser
+        {
+            public string Parametro { get; set; }
+            public string Valore { get; set; }
+        }
+    }
+}
